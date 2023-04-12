ff235f121a630c1b2e3f7d1e9ac3331ed236b1e6
Bijay Adhikari <71753686+Bijay-Adhikari@users.noreply.github.com>
2023-04-13 00:01:42 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function() {
+  $('.hero').ripples({
+    resolution: 512,
+    dropRadius: 20,
+    perturbance: 0.04,
+  });
+
+  $('.hero').mouseenter(function(event) {
+    $('.hero').ripples('drop', event.clientX, event.clientY, 20, 0.05);
+  });
+});
+
+// for hamburger menu
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+hamburger.addEventListener('click', function() {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+var navLinks = document.querySelectorAll("nav a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function() {
+    menu.classList.toggle('active');
+    hamburger.classList.remove('active')
+    
+  });
+}
+
+// for displaying live date(year)
+const current = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = current;
